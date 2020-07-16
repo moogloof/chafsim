03e33f1390757b3c450c4ae49cb5af382bea3bb0
Ethan Yun <ethanyun@ohs.stanford.edu>
2020-07-15 19:51:28 -0700

    Added basic info update function and widgets

--- src/main.py
+++ src/main.py
@@ -10,17 +10,20 @@ class Window(Tk):
         self.title("Charges and Fields")
         self.resizable(0, 0)
 
+        p1 = models.Particle(100, 300)
+        p2 = models.Particle(700, 300, -1)
+        system = models.System([p1, p2])
+
         self.canvas = Canvas(self, width=800, height=600, bg="black", bd=0, highlightthickness=0)
-        self.info_frame = InfoFrame(self, bd=0, highlightthickness=0)
+        self.info_frame = InfoFrame(system, self, bd=0, highlightthickness=0)
+
+        self.canvas.bind("<Motion>", self.info_frame.update)
 
         self.canvas.grid(row=0, column=0)
         self.info_frame.grid(row=0, column=1)
 
         self.update()
 
-        p1 = models.Particle(100, 300)
-        p2 = models.Particle(700, 300, -1)
-        system = models.System([p1, p2])
         self.display(system)
 
         self.mainloop()
@@ -50,8 +53,34 @@ class Window(Tk):
 
 
 class InfoFrame(Frame):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, field, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.x_label_var = StringVar()
+        self.x_label = Label(self, textvariable=self.x_label_var, width=15)
+        self.y_label_var = StringVar()
+        self.y_label = Label(self, textvariable=self.y_label_var, width=15)
+        self.field_label_var = StringVar()
+        self.field_label = Label(self, textvariable=self.field_label_var, width=30)
+
+        self.x_label.grid(row=0, column=0)
+        self.y_label.grid(row=0, column=1)
+        self.field_label.grid(row=1, column=0, columnspan=2)
+
+        self.field = field
+
+    def update(self, mov):
+        self.x_label_var.set(f"X: {mov.x}")
+        self.y_label_var.set(f"Y: {mov.y}")
+
+        f = self.field.get_field(mov.x, mov.y)
+        if f:
+            f = self.field.distance((0, 0), f)
+            f = round(f)
+        else:
+            f = "NaN"
+
+        self.field_label_var.set(f"Field: {f}")
 
 
 if __name__ == "__main__":
